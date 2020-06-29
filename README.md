4f14357cb075392421911b423b5f5645f9fe6729
Alex Wakeman <wakemana@gmail.com>
2020-06-29 13:59:25 +0100

    Fix any remaining bugs, basic flow is working

--- README.md
+++ README.md
@@ -8,3 +8,14 @@
 
 Uses Spotify OAuth to access their API. You will need to get an API client ID and secret to perform oAuth,
 and update the example-config.json, and `server/oauth.js` file to reflect this.
+
+
+Image One: 
+![screenshot1]
+
+Image Two: 
+![screenshot2]
+
+
+[screenshot1]: ./images/screenshot1.png "Blur Screesnhot"
+[screenshot2]: ./images/screenshot2.png "Shed 7 Screesnhot"
--- src/js/components/search-input.component.jsx
+++ src/js/components/search-input.component.jsx
@@ -4,16 +4,18 @@ import {Props} from '../classes/props';
 export function SearchInputComponent({searchTerm, artist, handleSearch, handleSearchTermUpdate, clearSession}) {
     const clearBtnClass = artist.id ? 'clear-session' : 'hidden clear-session';
 
+    function clearScene(evt) {
+        Props.sceneInst.clearGraph();
+        Props.sceneInst.clearAddress();
+        clearSession(evt);
+    }
+
     return (
         <div className="search-form-container">
             <form className="artist-search" onSubmit={(evt) => handleSearch(evt, searchTerm)}>
-                <input type="text" id="search-input" placeholder="e.g. Jimi Hendrix" value={searchTerm} onChange={handleSearchTermUpdate}/>
+                <input type="text" id="search-input" placeholder="e.g. Jimi Hendrix" value={searchTerm} onChange={handleSearchTermUpdate} />
                 <button type="submit" onClick={(evt) => handleSearch(evt, searchTerm)}>Go</button>
-                <button className={clearBtnClass} type="button" onClick={(evt) => {
-                    clearSession(evt);
-                    Props.sceneInst.clearGraph();
-                }}>Clear Session
-                </button>
+                <button className={clearBtnClass} type="button" onClick={(evt) => clearScene(evt)}>Clear Session</button>
             </form>
         </div>
     );
